d594f55f836bd0f43086997e07d499069bd07609
ErikBorgogno <erikborgo.20@gmail.com>
2022-07-11 14:19:51 +0200

    init and done project

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//es per casa
+
+const lista = [
+    "Sale q.b",
+    "Pepe q.b",
+    "Sale q.b",
+    "Uova 4",
+    "Formaggio Grattuggiato 200g",
+    "Cipolle 1",
+    "Zucchine 3/4"
+];
+
+const listaDom = document.getElementById('listaSpesa');
+let x = 0;
+
+while(x < lista.length){
+    listaDom.innerHTML += `<li>${lista[x]}</li>`;
+    x++;
+}
+
+console.log(x);
